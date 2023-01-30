358e2e803f2ea574f20bd085a8f6f4283c19228f
rivkabn <rivkabn81@gmail.com>
2023-01-30 19:36:33 +0200

    a little color

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css"
+const Header = () => {
+    const HeaderStyle = { color: "violet", marginTop: "15px", margin: "20px" }
+    return (
+        <div className='pink'><h1 style={HeaderStyle}>Je t'aime si tu m'aime et j'investies pour  toi si tu investies pour moi,<br></br> est-ce la toute la reussite de notre couple?</h1></div>
+    )
+}
+
+export default Header
