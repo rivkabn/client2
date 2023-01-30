59a432ca45cd1695e4c34564fb00795966e52e0e
rivkabn <rivkabn81@gmail.com>
2023-01-30 19:43:40 +0200

    small fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import "./Header.css"
 const Header = () => {
     const HeaderStyle = { color: "violet", marginTop: "15px", margin: "20px" }
     return (
-        <div className='pink'><h1 style={HeaderStyle}>Je t'aime si tu m'aime et j'investies pour  toi si tu investies pour moi,<br></br> est-ce  toute la reussite de notre couple?</h1></div>
+        <div className='pink'><h1 style={HeaderStyle}>Je t'aime si tu m'aime et j'investis pour  toi si tu investis pour moi,<br></br> Est-ce  toute la reussite de notre couple?</h1></div>
     )
 }
 
